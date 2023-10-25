4da397f6ae69ca5bca991e3fdba80b6ecc8996be
hfbhfb <602963123@qq.com>
2023-10-25 17:47:25 +0800

    helm安装社区版本kube-prometheus-stack

---
+++ Makefile
@@ -0,0 +1,17 @@
+
+helmAppName=prometheus1
+
+build-template:
+	rm -Rf outdir-${helmAppName}
+	helm template kube-prometheus-stack/ --namespace monitoring --values ./values.yaml --name-template ${helmAppName} --output-dir outdir-${helmAppName}
+
+install:
+	helm install kube-prometheus-stack/ --namespace monitoring --values ./values.yaml --name-template ${helmAppName} 
+
+uninstall:
+	helm uninstall --namespace monitoring ${helmAppName} 
+
+
+all:
+	bash helmget.sh
+
---
+++ helmget.sh
@@ -0,0 +1,18 @@
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+# helm repo update prometheus-community
+
+
+
+# 查找一下对应的helm仓库有哪些包 list
+helm search repo prometheus-community -l
+
+# 安装指定版本
+fixVersion=47.3.0 
+helm pull prometheus-community/kube-prometheus-stack --version $fixVersion --untar
+
+
+
+# helm template 模板运算
+helmAppName=prometheus1
+helm template kube-prometheus-stack/ --namespace monitoring --values ./values.yaml --name-template $helmAppName --output-dir outdir-$helmAppName
